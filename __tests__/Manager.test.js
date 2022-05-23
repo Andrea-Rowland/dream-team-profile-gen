2591fc43dabe651ffa89a7b9257aa07a1e336261
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-05-23 17:14:24 -0600

    engineer js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,70 +1,28 @@
 const Manager = require("../lib/Manager");
 
 test('creates instance of manager', () => {
-    const mgr = new Manager();
+    const emp = new Manager();
 
-    expect(typeof(mgr)).toBe("object");
-});
-
-test('saves name of Manager', () => {
-    const name = 'Ollie';
-    const mgr = new Manager(name);
-
-    expect(mgr.name).toBe('Ollie');
-});
-
-test('assigns id to Manager', () => {
-    const id = '1';
-    const mgr = new Manager('Ollie', id);
-
-    expect(mgr.id).toBe('1');
-});
-
-test('saves email of Manager', () => {
-    const email = 'mrolliepants@dog.com';
-    const mgr = new Manager('Ollie', 1, email);
-
-    expect(mgr.email).toBe('mrolliepants@dog.com');
+    expect(typeof(emp)).toBe("object");
 });
 
 test('saves office number of Manager', () => {
-    const officeNumber = "2";
-    const mgr = new Manager('Ollie', 1, "mrolliepants@dog.com", officeNumber);
+    const officeNumber = '2';
+    const emp = new Manager('Ollie', 1, 'mrolliepants@dog.com', officeNumber);
 
-    expect(mgr.officeNumber).toBe('2');
+    expect(emp.officeNumber).toBe(officeNumber);
 });
 
-test('retrieve manager name', () => {
-    const name = 'Ollie';
-    const mgr = new Manager('Ollie');
-
-    expect(mgr.getName()).toBe(name);
-});
-
-test('retrieve manager id', () => {
-    const id = '1';
-    const mgr = new Manager ('Ollie', id);
-
-    expect(mgr.getId()).toBe(id);
-});
-
-test('retrieve email', () => {
-    const email = 'mrolliepants@dog.com';
-    const mgr = new Manager ('Ollie', 1, email);
-
-    expect(mgr.getEmail()).toBe(email);
-});
-
-test('get role', () => {
+test('show what their role is', () => {
     const role = 'Manager';
-    const mgr = new Manager ('Ollie, 1, "mrolliepants@dog.com', role);
+    const emp = new Manager ('Ollie, 1, "mrolliepants@dog.com', role);
 
-    expect(mgr.getRole()).toBe
+    expect(emp.getRole()).toBe(role);
 });
 
 test('gets the office number', () => {
     const officeNumber = '2';
-    const mgr = new Manager('Ollie', 1, 'mrolliepants@dog.com', 'Manager', officeNumber);
+    const emp = new Manager('Ollie', 1, 'mrolliepants@dog.com', 'Manager', officeNumber);
 
-    expect(mgr.getOfficeNumber()).toBe(officeNumber);
+    expect(emp.getOfficeNumber()).toBe('2');
 });
